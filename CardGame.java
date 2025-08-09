da492992c5e5af3891ce7afe129646110e5aeeee
AnshikaaTomar <anshika.singh5417@gmail.com>
2025-08-09 16:05:31 +0530

    Main File

---
+++ CardGame.java
@@ -0,0 +1,11 @@
+package POKER;
+//import java.util.*;
+
+public class CardGame {
+
+    public static void main(String[] args) {
+        
+        PokerGame fiveCards = new PokerGame(6, 5);
+        fiveCards.startPlay();
+    }
+}
